63414aa22a64c1c1f337159f0a0a34fdbb351979
Germanoto5 <mylogc@gmail.com>
2024-06-10 18:28:23 +0200

    Alerta detalle del producto

---
+++ lib/pages/menu/product_detail_popup_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:texasgrill_app/models/product_menu.dart';
+
+class ProductDetail extends StatelessWidget {
+  final ProductMenu product;
+  const ProductDetail({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      contentPadding: EdgeInsets.only(top:0, bottom: 30, left: 0, right: 0),
+      backgroundColor: Theme.of(context).colorScheme.inverseSurface,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Center(
+                    child: Image.asset(
+                      'assets/images/burguer.png',
+                      width: 120,
+                      height: 120,
+                    ),
+                  ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(
+                      Icons.close,
+                      color: Theme.of(context).colorScheme.primary,
+                    )),
+              ),
+              /*product.urlImagen != null && product.urlImagen!.isNotEmpty? 
+              Center(
+                child: Image.network(
+                  product.urlImagen!,
+                  width: 120,
+                  height: 120,
+                ),
+              ) : SizedBox(),*/
+            ],
+          ),
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+                    textAlign: TextAlign.center,
+                    product.nombre!,
+                    style: TextStyle(
+                        fontSize: 22, color: Theme.of(context).colorScheme.secondary,
+                        ),
+                  ),
+          ),
+                const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                textAlign: TextAlign.center,
+                product.ingredientes != null && product.ingredientes!.isNotEmpty
+                    ? product.ingredientes!
+                    : "Pregunta los ingredientes en el restaurante.",
+                style: TextStyle(
+                    fontSize: 14, color: Theme.of(context).colorScheme.tertiary),
+              ),
+            ),
+          
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/menu/product_item_widget.dart
+++ lib/pages/menu/product_item_widget.dart
@@ -1,60 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:texasgrill_app/models/product_menu.dart';
+import 'package:texasgrill_app/pages/menu/product_detail_popup_widget.dart';
 
 class ProductWidget extends StatelessWidget {
   final ProductMenu product;
   const ProductWidget({super.key, required this.product});
 
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      child: Container(
-        padding: const EdgeInsets.all(15),
-        width: MediaQuery.of(context).size.width/2 -20,
-        height: 250,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Theme.of(context).colorScheme.inverseSurface,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Center(
-              child: Image.asset(
-                'assets/images/burguer.png',
-                width: 120,
-                height: 120,
-              ),
-            ),
-            /*product.urlImagen != null && product.urlImagen!.isNotEmpty? 
-            Center(
-              child: Image.network(
-                product.urlImagen!,
-                width: 120,
-                height: 120,
-              ),
-            ) : SizedBox(),*/
-            const SizedBox(
-              height: 10,
-            ),
-            Text(
-              product.nombre!.toUpperCase(),
-              style: TextStyle(
-                  color: Theme.of(context).colorScheme.secondary, fontSize: 16),
-            ),
-            const Spacer(),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Text(
-                  "${product.precio!.toString()} €",
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.tertiary,
-                      fontSize: 20),
+      child: InkWell(
+        onTap: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return ProductDetail(product: product);
+              },
+            );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          width: MediaQuery.of(context).size.width/2 -20,
+          height: 250,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Theme.of(context).colorScheme.inverseSurface,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Center(
+                child: Image.asset(
+                  'assets/images/burguer.png',
+                  width: 120,
+                  height: 120,
                 ),
-            ),
-          ],
+              ),
+              /*product.urlImagen != null && product.urlImagen!.isNotEmpty? 
+              Center(
+                child: Image.network(
+                  product.urlImagen!,
+                  width: 120,
+                  height: 120,
+                ),
+              ) : SizedBox(),*/
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                product.nombre!.toUpperCase(),
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary, fontSize: 16),
+              ),
+              const Spacer(),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                    "${product.precio!.toString()} €",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.tertiary,
+                        fontSize: 20),
+                  ),
+              ),
+            ],
+          ),
         ),
       ),
     );
